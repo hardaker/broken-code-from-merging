9b685f934e4237d6cf78279f6f46e53f561a11b2
Wes Hardaker <opensource@hardakers.net>
2021-10-10 10:10:05 -0700

    initial hello world implementation

---
+++ test.py
@@ -0,0 +1,18 @@
+#
+# Let's create a function that simply prints an argument
+#
+
+def print_greeting(to_what):
+    print(f"hello {to_what}")
+
+#
+# and then creating something use that greeting
+#
+def main():
+    print_greeting("world")
+
+#
+# and call it
+#
+main()
+
