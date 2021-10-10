f6ab3e0995dc7af4a349747b4ab3187d39ed44a3
Wes Hardaker <opensource@hardakers.net>
2021-10-10 10:10:32 -0700

    greet from 1678

--- test.py
+++ test.py
@@ -2,14 +2,14 @@
 # Let's create a function that simply prints an argument
 #
 
-def print_greeting(to_what):
-    print(f"hello {to_what}")
+def print_greeting(to_what, from_who):
+    print(f"hello {to_what}, I'm {from_who}")
 
 #
 # and then creating something use that greeting
 #
 def main():
-    print_greeting("world")
+    print_greeting("world", "1678")
 
 #
 # and call it
